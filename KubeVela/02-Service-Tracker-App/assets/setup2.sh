d06793a5cd9f847fcd46ed2d4318c65f48a10929
sreejith-rajashekaran <sreejith.rajashekaran@hotmail.com>
2024-12-03 19:44:03 +0530

    more progress

--- KubeVela/02-Service-Tracker-App/assets/setup2.sh
+++ KubeVela/02-Service-Tracker-App/assets/setup2.sh
@@ -6,4 +6,5 @@ mkdir -p KubeVelaManifest
 
 
 mv /root/app.yaml /root/KubeVelaManifest/app.yaml
-
+mv /root/enhanced-webservice.yaml /root/KubeVelaManifest/enhanced-webservice.yaml
+mv /root/secret.yaml /root/KubeVelaManifest/secret.yaml
