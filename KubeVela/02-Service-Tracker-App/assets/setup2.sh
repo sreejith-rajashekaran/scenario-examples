1eea44f58d316f77ac0081fbed9a18d3c9833c46
sreejith-rajashekaran <sreejith.rajashekaran@hotmail.com>
2024-12-03 17:43:15 +0530

    Second Exercise 8

--- KubeVela/02-Service-Tracker-App/assets/setup2.sh
+++ KubeVela/02-Service-Tracker-App/assets/setup2.sh
@@ -1,7 +1,7 @@
-mkdir KubeVelaManifest
+
 mv /root/app.yml KubeVelaManifest/app.yml
 mv /root/enhanced-webservice.yml KubeVelaManifest/enhanced-webservice.yml
 mv /root/secret.yml KubeVelaManifest/secret.yml
 
-echo "Configuration moved!"
+echo "Configuration moved again!"
 
--- KubeVela/02-Service-Tracker-App/setup.sh
+++ KubeVela/02-Service-Tracker-App/setup.sh
@@ -1,3 +1,7 @@
 curl -fsSl https://kubevela.io/script/install.sh | bash
 
 vela install
+
+mkdir KubeVelaManifest
+
+echo "Configuration moved!"
